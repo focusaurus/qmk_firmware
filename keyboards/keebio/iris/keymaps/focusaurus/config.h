5b27db1052590e0094e1e36835f4d961717c9caf
Peter Lyons <pete.lyons@mailchimp.com>
2020-11-20 11:22:54 -0700

    tapping term 200

--- keyboards/keebio/iris/keymaps/focusaurus/config.h
+++ keyboards/keebio/iris/keymaps/focusaurus/config.h
@@ -19,6 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define USE_I2C
 #define EE_HANDS
-#define TAPPING_TERM 500
+#define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
