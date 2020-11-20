54169fb4fc28ae1c0360fcde25f678f562b33f82
Peter Lyons <pete.lyons@mailchimp.com>
2020-11-19 19:58:23 -0700

    Got my basic 4 layers set up

--- keyboards/keebio/iris/keymaps/focusaurus/keymap.c
+++ keyboards/keebio/iris/keymaps/focusaurus/keymap.c
@@ -3,76 +3,74 @@
 
 #define _DVORAK 0
 #define _NAVPAREN 1
-#define _RAISE 2
-#define _ADJUST 3
+#define _NUM 2
+#define _KEEBNAV 3
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
+  DVORAK = SAFE_RANGE,
   NAVPAREN,
-  RAISE,
-  ADJUST,
+  NUM,
+  KEEBNAV,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_DVORAK] = LAYOUT(
   // row A
-  KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_EQL, KC_BSLS,
+  KC_GESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, /*right*/ KC_F6, KC_F7, KC_F8, KC_F9, KC_EQL, KC_BSLS,
   // row B
-  KC_TAB, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH,
+  KC_TAB, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, /*right*/ KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH,
   // row C
-  LCTL_T(KC_ESC), MT(MOD_LGUI | MOD_LALT, KC_A), KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, MT(MOD_LGUI | MOD_LALT, KC_S), KC_MINS,
+  LCTL_T(KC_ESC), MT(MOD_LGUI | MOD_LALT, KC_A), KC_O, KC_E, KC_U, KC_I, /*right*/ KC_D, KC_H, KC_T, KC_N, MT(MOD_LGUI | MOD_LALT, KC_S), KC_MINS,
   // row D
-  OSM(MOD_LSFT), KC_SCLN, KC_Q, KC_J, KC_K, KC_X, RESET, KC_MUTE, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT,
+  OSM(MOD_LSFT), KC_SCLN, KC_Q, KC_J, KC_K, KC_X, /*rotary*/ RESET, KC_MUTE, /*right*/ KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT,
   // row E
-  MT(MOD_LGUI | MOD_LALT, KC_BSPC), KC_LGUI, LT(_NAVPAREN,KC_ENT), LT(3, KC_ENT), LT(2,KC_SPC), KC_RALT
+  MT(MOD_LGUI | MOD_LALT, KC_BSPC), KC_LGUI, LT(_NAVPAREN,KC_ENT), /*right*/ LT(3, KC_ENT), LT(2,KC_SPC), KC_RALT
   ),
 
   [_NAVPAREN] = LAYOUT(
   // row A
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /*right*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   // row B
-  KC_TRNS, LGUI(KC_TAB), KC_BSPC, KC_SPC, KC_DEL, KC_PGUP, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, LGUI(KC_TAB), KC_BSPC, KC_SPC, KC_DEL, KC_PGUP, KC_TRNS, /*right*/ KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS, KC_TRNS,
   // row C
-  KC_TRNS, KC_TAB, KC_LEFT, KC_UP, KC_RIGHT, KC_ENT, KC_TRNS, KC_LPRN, KC_RPRN, KC_GRV, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TAB, KC_LEFT, KC_UP, KC_RIGHT, KC_ENT, /*right*/ KC_TRNS, KC_LPRN, KC_RPRN, KC_GRV, KC_TRNS, KC_TRNS,
   // row D
-  KC_TRNS, KC_TRNS, KC_HOME, KC_DOWN, KC_END, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_HOME, KC_DOWN, KC_END, KC_PGDN, /*rotary*/ KC_TRNS, KC_TRNS, /*right*/ KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS,
   // row E
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  KC_TRNS, KC_TRNS, KC_TRNS, /*right*/ KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
-  [_RAISE] = LAYOUT(
-  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_TOG, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_MOD, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_UNDS,                            KC_EQL,  KC_HOME, RGB_HUI, RGB_SAI, RGB_VAI, _______,
-  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, KC_LPRN,          _______, KC_PLUS, KC_END,  RGB_HUD, RGB_SAD, RGB_VAD, _______,
-  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   _______, _______, _______
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  [_NUM] = LAYOUT(
+  // row A
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /*right*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  // row B
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /*right*/ KC_COMM, KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS,
+  // row C
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /*right*/ KC_DOT, KC_4, KC_5, KC_6, KC_0, KC_TRNS,
+  // row D
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /*rotary*/ KC_TRNS, KC_TRNS, /*right*/ LSFT(KC_COLN), KC_1,  KC_2,  KC_3, KC_TRNS, KC_TRNS,
+  // row E
+  KC_TRNS, KC_TRNS, KC_TRNS, /*right*/ KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
-  [_ADJUST] = LAYOUT(
-  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
-  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
-  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   _______, _______, _______
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  [_KEEBNAV] = LAYOUT(
+  // row A
+  RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /*right*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  // row B
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_VAI, KC_TRNS, /*right*/ KC_PGUP, KC_BSPC, KC_SPC, KC_DEL, LGUI(KC_TAB), KC_TRNS,
+  // row C
+  KC_TRNS, KC_TRNS, RGB_RMOD, RGB_TOG, RGB_MOD, KC_TRNS, /*right*/ KC_ENT, KC_LEFT, KC_UP, KC_RIGHT, KC_TAB, KC_TRNS,
+  // row D
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_VAD, KC_TRNS, /*rotary*/ KC_TRNS, KC_TRNS, /*right*/ KC_PGDN, KC_HOME, KC_DOWN, KC_END, KC_TRNS, KC_TRNS,
+  // row E
+  KC_TRNS, KC_TRNS, KC_TRNS, /*right*/ KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QWERTY:
+    case DVORAK:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_DVORAK);
       }
@@ -81,28 +79,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case NAVPAREN:
       if (record->event.pressed) {
         layer_on(_NAVPAREN);
-        update_tri_layer(_NAVPAREN, _RAISE, _ADJUST);
+        update_tri_layer(_NAVPAREN, _NUM, _KEEBNAV);
       } else {
         layer_off(_NAVPAREN);
-        update_tri_layer(_NAVPAREN, _RAISE, _ADJUST);
+        update_tri_layer(_NAVPAREN, _NUM, _KEEBNAV);
       }
       return false;
       break;
-    case RAISE:
+    case NUM:
       if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_NAVPAREN, _RAISE, _ADJUST);
+        layer_on(_NUM);
+        update_tri_layer(_NAVPAREN, _NUM, _KEEBNAV);
       } else {
-        layer_off(_RAISE);
-        update_tri_layer(_NAVPAREN, _RAISE, _ADJUST);
+        layer_off(_NUM);
+        update_tri_layer(_NAVPAREN, _NUM, _KEEBNAV);
       }
       return false;
       break;
-    case ADJUST:
+    case KEEBNAV:
       if (record->event.pressed) {
-        layer_on(_ADJUST);
+        layer_on(_KEEBNAV);
       } else {
-        layer_off(_ADJUST);
+        layer_off(_KEEBNAV);
       }
       return false;
       break;
