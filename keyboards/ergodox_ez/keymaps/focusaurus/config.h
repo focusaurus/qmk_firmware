2202d3163cf3c0e5b9c6a9c9a6787f0baa68b506
Peter Lyons <pete.lyons@mailchimp.com>
2021-03-05 07:42:36 -0700

    More home row mods and combo for escape

--- keyboards/ergodox_ez/keymaps/focusaurus/config.h
+++ keyboards/ergodox_ez/keymaps/focusaurus/config.h
@@ -18,4 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
 #define PERMISSIVE_HOLD
+// #define TAPPING_TERM 300
+#define COMBO_COUNT 1
