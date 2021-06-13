ed6884afe680f9ad9fa4ed27c14d19cff7bb128e
Peter Lyons <pete.lyons@mailchimp.com>
2021-06-13 15:03:23 -0600

    Use top thumb cluster keys instead of bottom

--- keyboards/squeezebox/v2105/keymaps/focusaurus/keymap.c
+++ keyboards/squeezebox/v2105/keymaps/focusaurus/keymap.c
@@ -106,93 +106,127 @@ enum layer_names {
 
     DVORAK,
     MACOS,
-    NAVNUM,
+    EDIT,
+    NUMBERS,
     BANG,
     KBFN
 
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
-    [DVORAK] = LAYOUT(
 
-        // TODO 1-finger combos for uk, ej, oq
-        KC_QUOTE, KC_COMMA /*TD(TD_COMMA)*/, LALT_T(KC_DOT), LCTL_T(KC_P), KC_Y,
-        MT(MOD_LGUI | MOD_LALT, KC_A), KC_O, KC_E, KC_U, KC_I,
-        KC_SCLN /*TD(TD_SEMI)*/, KC_Q, KC_J, KC_K, KC_X,
-        KC_ESC, KC_BSPC, OSM(MOD_LSFT),
-        // OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT),
-        LT(NAVNUM, LEADER), LT(KBFN, FUZZBALL), MT(MOD_LGUI, KC_DEL),
+// TODO make a dedicated LAYOUT_fingers that leaves thumb clusters transparent
+/* Base */
+[DVORAK] = LAYOUT(
 
-        // TODO 1-finger combos for hm, tw, nv, sz
-        KC_F, RCTL_T(KC_G), LALT_T(KC_C), KC_R, KC_L,
-        KC_D, KC_H, KC_T, KC_N, KC_S,
-        KC_B, KC_M, KC_W, KC_V, KC_Z,
-        // TODO if top row mods work out can use these for something else
-        // KC_3, KC_2, KC_1,
-        // KC_6, KC_5, KC_4
-        OSM(MOD_RSFT), LT(KBFN, KC_BSPC), KC_DEL,
-        OSM(MOD_RGUI), KC_ENT, LT(BANG, KC_SPC)
+// TODO 1-finger combos for uk, ej, oq
+// left hand
+KC_QUOTE, KC_COMMA /*TD(TD_COMMA)*/, LALT_T(KC_DOT), LCTL_T(KC_P), KC_Y,
+MT(MOD_LGUI | MOD_LALT, KC_A), KC_O, KC_E, KC_U, KC_I,
+KC_SCLN /*TD(TD_SEMI)*/, KC_Q, KC_J, KC_K, KC_X,
+KC_ESC, LT(KBFN, KC_BSPC), LT(EDIT, LEADER),
+// OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT),
+FUZZBALL, OSM(MOD_LSFT), MT(MOD_LGUI, KC_DEL),
 
-            ),
-    [MACOS] = LAYOUT(
+// TODO 1-finger combos for hm, tw, nv, sz
+// right hand
+KC_F, RCTL_T(KC_G), LALT_T(KC_C), KC_R, KC_L,
+KC_D, KC_H, KC_T, KC_N, KC_S,
+KC_B, KC_M, KC_W, KC_V, KC_Z,
+// TODO if top row mods work out can use these for something else
+// KC_3, KC_2, KC_1,
+// KC_6, KC_5, KC_4
+LT(NUMBERS, KC_SPC), LT(KBFN, KC_BSPC), KC_DEL,
+OSM(MOD_RGUI), LT(BANG, KC_ENT), LT(NUMBERS, KC_SPC)
 
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, OSM(MOD_LGUI), KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS,
+), [MACOS] = LAYOUT(
 
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, OSM(MOD_LGUI),
-            KC_TRNS, KC_TRNS, KC_TRNS
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, OSM(MOD_LGUI), KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS,
 
-            ),
-    [NAVNUM] = LAYOUT(
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, OSM(MOD_LGUI),
+KC_TRNS, KC_TRNS, KC_TRNS
 
-            KC_TRNS, KC_BSPC, KC_SPC, KC_DEL, KC_PGUP,
-            KC_TAB, KC_LEFT, KC_UP, KC_RIGHT, KC_ENT,
-            LCTL(KC_A), KC_HOME, KC_DOWN, KC_END, KC_PGDN,
-            KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS,
+), [EDIT] = LAYOUT(
 
-            KC_COMM, KC_7, KC_8, KC_9, KC_NO,
-            KC_DOT, KC_4, KC_5, KC_6, KC_0,
-            KC_COLON, KC_1, KC_2, KC_3, KC_NO,
-            KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS
+// left hand
+LGUI(KC_TAB), KC_BSPC, KC_SPC, KC_DEL, KC_PGUP,
+KC_TAB, KC_LEFT, KC_UP, KC_RIGHT, KC_ENT,
+LCTL(KC_TAB), KC_HOME, KC_DOWN, KC_END, KC_PGDN,
+KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS,
 
-            ),
-    [BANG] = LAYOUT(
+// right hand
+RSFT(KC_EQL), KC_LCBR, KC_RCBR, LSFT(KC_GRV), KC_EQL,
+KC_EQL, KC_LPRN, KC_RPRN, KC_SLSH, KC_GRV,
+KC_BSLS, KC_LBRC /*TD(TD_LBRC)*/, KC_RBRC /*TD(TD_RBRC)*/, KC_PIPE, RSFT(KC_MINUS),
+KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS
 
-            KC_LT, KC_GT, KC_LCBR, KC_RCBR, KC_PIPE,
-            KC_MINUS, KC_UNDERSCORE, KC_LPRN, KC_RPRN, KC_SCLN,
-            KC_QUES, KC_EXCLAIM, KC_LBRC, KC_RBRC, KC_BSLS,
-            KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS,
+), [NUMBERS] = LAYOUT(
 
-            LSFT(KC_EQL), LSFT(KC_7), LSFT(KC_8), KC_NO, KC_TILDE,
-            KC_EQL, LSFT(KC_4), LSFT(KC_5), LSFT(KC_6), KC_SLSH,
-            KC_GRV, LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), KC_COLON,
-            KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS
+// HEADS UP: This is a copy of the left hand from the EDIT
+// layer because QMK layers are dumb and don't really
+// work in any kind of reasonable way.
+// Without this copying, the default layer of DVORAK would fire
+// which is not what I want
+// left hand
+LGUI(KC_TAB), KC_BSPC, KC_SPC, KC_DEL, KC_PGUP,
+KC_TAB, KC_LEFT, KC_UP, KC_RIGHT, KC_ENT,
+LCTL(KC_TAB), KC_HOME, KC_DOWN, KC_END, KC_PGDN,
+KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS,
 
-            ),
-    [KBFN] = LAYOUT(
+// right hand
+KC_COMMA, KC_7, KC_8, KC_9, KC_MINUS,
+KC_DOT, KC_4, KC_5, KC_6, KC_0,
+KC_COLON, KC_1, KC_2, KC_3, LSFT(KC_MINUS),
+KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS
 
-            KC_NO, KC_NO, KC_NO, KC_VOLU, KC_NO,
-            KC_CAPSLOCK, KC_NO, KC_NO, KC_MUTE, RESET,
-            TG(MACOS), KC_NO, KC_NO, KC_VOLD, KC_NO,
-            KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS,
+), [BANG] = LAYOUT(
 
-            KC_NO, KC_F7, KC_F8, KC_F9, KC_F12,
-            KC_NO, KC_F4, KC_F5, KC_F6, KC_F11,
-            KC_NO, KC_F1, KC_F2, KC_F3, KC_F10,
-            KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS
+// HEADS UP: This is a copy of the left hand from the EDIT
+// layer because QMK layers are dumb and don't really
+// work in any kind of reasonable way.
+// Without this copying, the default layer of DVORAK would fire
+// which is not what I want
+// left hand
+LGUI(KC_TAB), KC_BSPC, KC_SPC, KC_DEL, KC_PGUP,
+KC_TAB, KC_LEFT, KC_UP, KC_RIGHT, KC_ENT,
+LCTL(KC_TAB), KC_HOME, KC_DOWN, KC_END, KC_PGDN,
+KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS,
 
-            ),
+// right hand
+LSFT(KC_EQL), LSFT(KC_7), LSFT(KC_8), KC_SLSH, KC_PIPE,
+KC_EQL, LSFT(KC_4), LSFT(KC_5), LSFT(KC_6), KC_MINUS,
+// TODO maybe combo for a pair of curly braces for bash vars?
+KC_GRV, LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), KC_QUES,
+KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS
+
+), [KBFN] = LAYOUT(
+
+// left hand
+KC_NO, KC_NO, KC_NO, KC_VOLU, KC_NO,
+KC_CAPSLOCK, KC_NO, KC_NO, KC_MUTE, RESET,
+TG(MACOS), KC_NO, KC_NO, KC_VOLD, KC_NO,
+KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS,
+
+// right hand
+KC_NO, KC_F7, KC_F8, KC_F9, KC_F12,
+KC_NO, KC_F4, KC_F5, KC_F6, KC_F11,
+KC_NO, KC_F1, KC_F2, KC_F3, KC_F10,
+KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS
+
+),
 };
