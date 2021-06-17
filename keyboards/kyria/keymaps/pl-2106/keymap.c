6f1dafaf769bcb5159b1b74dce5c2d00f4ffcaa6
Peter Lyons <pete.lyons@mailchimp.com>
2021-06-16 20:19:03 -0600

    update kyria pl-2106 with thumb clusters and combos

--- keyboards/kyria/keymaps/pl-2106/keymap.c
+++ keyboards/kyria/keymaps/pl-2106/keymap.c
@@ -15,6 +15,51 @@
  */
 #include QMK_KEYBOARD_H
 
+enum combos {
+
+  COMBO_BACKSPACE,
+  COMBO_BRACKETS,
+  COMBO_ESCAPE,
+  COMBO_PARENS,
+  COMBO_PIPE,
+
+};
+
+const uint16_t PROGMEM combo_backspace[] = {KC_NO, KC_NO, COMBO_END};
+const uint16_t PROGMEM combo_brackets[] = {KC_J, KC_E, COMBO_END};
+const uint16_t PROGMEM combo_escape[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM combo_parens[] = {KC_U, KC_K, COMBO_END};
+const uint16_t PROGMEM combo_pipe[] = {KC_DOT, KC_P, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+
+  [COMBO_BACKSPACE] = COMBO(combo_backspace, KC_BSPC),
+  [COMBO_BRACKETS] = COMBO_ACTION(combo_brackets),
+  [COMBO_ESCAPE] = COMBO(combo_escape, KC_ESC),
+  [COMBO_PARENS] = COMBO_ACTION(combo_parens),
+  [COMBO_PIPE] = COMBO(combo_pipe, KC_PIPE)
+
+};
+
+void process_combo_event(uint16_t combo_index, bool pressed) {
+  switch(combo_index) {
+    case COMBO_BRACKETS:
+      if (pressed) {
+        tap_code16(KC_LBRC);
+        tap_code16(KC_RBRC);
+        tap_code16(KC_LEFT);
+      }
+      break;
+    case COMBO_PARENS:
+      if (pressed) {
+        tap_code16(KC_LPRN);
+        tap_code16(KC_RPRN);
+        tap_code16(KC_LEFT);
+      }
+      break;
+  }
+}
+
 #define LEADER KC_F10
 #define FUZZBALL KC_F11
 #define SNIPPETS KC_F12
@@ -36,24 +81,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_TAB, KC_QUOTE, KC_COMMA /*TD(TD_COMMA)*/, LALT_T(KC_DOT), LCTL_T(KC_P), KC_Y,
 MT(MOD_LCTL, KC_ESC), MT(MOD_LGUI | MOD_LALT, KC_A), KC_O, KC_E, KC_U, KC_I,
 OSL(BANG), KC_SCLN /*TD(TD_SEMI)*/, KC_Q, KC_J, KC_K, KC_X, OSL(BANG), LT(KBFN, FUZZBALL),
-KC_NO, KC_NO, KC_BSPC, OSM(MOD_LSFT), LT(NAVNUM, LEADER),
+KC_LEFT, KC_RIGHT, KC_BSPC, LT(NAVNUM, LEADER), OSM(MOD_LSFT),
 
               KC_F, RCTL_T(KC_G), LALT_T(KC_C), KC_R, KC_L, KC_SLASH,
               KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINUS,
-KC_NO, KC_NO, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_NO,
-LT(KBFN, KC_ENT), LT(BANG, KC_SPC), OSM(MOD_RCTL), OSM(MOD_RGUI), OSM(MOD_LALT)
+OSM(MOD_RGUI), OSM(MOD_LALT), KC_B, KC_M, KC_W, KC_V, KC_Z, KC_NO,
+LT(KBFN, KC_ENT), LT(BANG, KC_SPC), OSM(MOD_RCTL), KC_LEFT, KC_RIGHT
 
 ), [MACOS] = LAYOUT_stack(
 
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-KC_TRNS, KC_TRNS, OSM(MOD_RGUI), OSM(MOD_RCTL), KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-KC_TRNS, KC_TRNS, OSM(MOD_RGUI), OSM(MOD_RCTL), KC_TRNS
+OSM(MOD_RCTL), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, OSM(MOD_RGUI), KC_TRNS, KC_TRNS
 
 ), [NAVNUM] = LAYOUT_stack(
 
